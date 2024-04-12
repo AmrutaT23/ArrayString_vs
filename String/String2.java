a207217985e0d92511e5f2a22541f310ff5b0a38
Amruta Thakare <154741561+AmrutaT23@users.noreply.github.com>
2024-04-12 21:46:21 +0530

    Sum of two string

---
+++ String/String2.java
@@ -0,0 +1,17 @@
+// Find the sum of two string
+
+public class String2 {
+    public static void main(String args[])
+    {
+        String s1 = "111";
+        String s2 = "222";
+        int sum = Integer.parseInt(s1) +Integer.parseInt(s2); //convert String to Integer
+        System.out.println("Sum of Two String is :"+sum);
+
+        String sum1 = String.valueOf(sum ); //convert Integer to String
+        System.out.println(sum1);
+
+
+         
+    }
+}
