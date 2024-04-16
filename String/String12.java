a5d9f787b1c730a94b725333a0f8662cdc71552c
Amruta Thakare <154741561+AmrutaT23@users.noreply.github.com>
2024-04-16 18:03:40 +0530

    Separate individual characters from a string

---
+++ String/String12.java
@@ -0,0 +1,14 @@
+// Write a program to separate individual characters from a string.
+
+public class String12 {
+    public static void main(String args[])
+    {
+        String s  = "Computer";
+        // String str ="";
+
+        for(int i=0; i<s.length(); i++)
+        {
+            System.out.print(s.charAt(i)+ " ");
+        }
+    }
+}
